18c8aa68c5b4868d5ef493c9cd104b72833921ea
Jon Neale <jon.neale@uswitch.com>
2010-10-15 17:25:48 +0100

    Added prime number generator

---
+++ sieve-of-eratosthenes.clj
@@ -0,0 +1,18 @@
+;;Clojure implementation of the Sieve of Eratosthenes method for finding primes
+;;Call (get-all-primes n) to get all primes between 2 and n
+
+(defn iter-primes
+  [current-list limit position]
+  (if (> (nth current-list position) (Math/sqrt limit))
+    current-list
+    (iter-primes (conj (filter (fn[x] (not= 0 
+                                      (rem x
+                                          (nth current-list position))))
+                               current-list)
+                       (nth current-list position))
+                 limit
+                 (+ 1 position))))
+                 
+(defn get-all-primes 
+  [n]
+  (iter-primes (range 2 n) n 0))
