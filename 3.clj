6aa15b0412751cc393f0552cdcc3b29b242bd670
Jon Neale <jon.neale@uswitch.com>
2010-10-16 12:33:31 +0100

    Added working and efficient solution for problem 3

--- 3.clj
+++ 3.clj
@@ -1,21 +1,17 @@
 (defn iter-primes
   [current-list limit position]
-  (if (> (nth current-list position) (Math/sqrt limit))
+  (if (>= position (count current-list))
     current-list
-    (iter-primes (conj (filter (fn[x] (not= 0 
-                                      (rem x
-                                          (nth current-list position))))
+    (iter-primes (conj (filter (fn[x] (and (not= 0 
+                                                (rem x
+                                                (nth current-list position)))
+                                          (= 0 (rem limit x))))
                                current-list)
                        (nth current-list position))
                  limit
                  (+ 1 position))))
                  
-(defn get-all-primes 
+(defn find-largest-prime-factor 
   [n]
-  (iter-primes (range 2 n) n 0))
-
-(defn prime-factors
-  [n]
-  (last (sort (filter (fn[x] (= 0 (rem n x)))
-    (get-all-primes (/ n 2))))))
+  (first (iter-primes (range 2 (Math/sqrt n)) n 0)))
   
